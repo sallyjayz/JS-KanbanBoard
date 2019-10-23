fb1d5040468b8cba866feb12004b0d799bf63c92
sallyjayz <salama.jatau@yahoo.com>
2019-10-23 11:18:10 +0100

    Completed module

--- js/kanban.js
+++ js/kanban.js
@@ -48,7 +48,7 @@ const create_item = () => {
 };
 
 document.querySelectorAll('.drop').forEach(element => {
-  document.addEventListener('drop', event => {
+  element.addEventListener('drop', event => {
     event.preventDefault();
     const id = event.dataTransfer.getData('text');
     event.target.appendChild(document.getElementById(id));
